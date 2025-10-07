dcbda3f546e97a4f048e382c81717eeaf3e02efc
paul_kniaz <pjkniaz@gmail.com>
2025-10-07 15:33:13 -0400

    board

---
+++ exercises.py
@@ -0,0 +1,17 @@
+class Game():
+    def __init__(self):
+        self.turn = 'X'
+        self.winner = None
+        self.tie = False
+        self.board = {
+            'a1': None, 'b1': None, 'c1': None,
+            'a2': None, 'b2': None, 'c2': None,
+            'a3': None, 'b3': None, 'c3': None,
+        }
+
+    def play_game():
+        return "Welcome!"
+    
+      
+game_instance = Game()
+print(game_instance.play_game())
